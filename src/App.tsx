6b82941eadeae23bb681b6d87cae015dea51ed58
DuongMinh <minhdtn@uniultra.xyz>
2024-04-16 14:27:16 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react"
 import Navbar from "./scences/navbar/navbar"
-
+import { SelectedPage } from "@/scences/shared/types"
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState<string>("Home")
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
   return (
     <div className="app bg-gray-20">
       <Navbar selectedPage={selectedPage}
