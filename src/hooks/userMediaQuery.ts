87f13b74171f362cb62860ce0ab523d72bb3d95f
DuongMinh <minhdtn@uniultra.xyz>
2024-04-16 17:37:49 +0700

    activeButton

--- src/hooks/userMediaQuery.ts
+++ src/hooks/userMediaQuery.ts
@@ -1,35 +1,34 @@
-import { match } from "assert";
 import { useState, useEffect } from "react";
 
-// const useMediaQuery = (query: string) => {
-//     const [matches, setMatches] = useState(false);
-//     useEffect(() => {
-//         const media = window.matchMedia(query);
-//         if (media.matches !== matches) {
-//             setMatches(media.matches);
-//         }
-//         const listener = () => setMatches(media.matches);
-//         window.addEventListener("resize", listener);
-//         return () => window.removeEventListener("resize", listener);
-//     }, [matches, query]);
+const useMediaQuery = (query: string) => {
+    const [matches, setMatches] = useState(false);
+    useEffect(() => {
+        const media = window.matchMedia(query);
+        if (media.matches !== matches) {
+            setMatches(media.matches);
+        }
+        const listener = () => setMatches(media.matches);
+        window.addEventListener("resize", listener);
+        return () => window.removeEventListener("resize", listener);
+    }, [matches, query]);
 
-//     return matches;
-// }
+    return matches;
+}
 
-// export default useMediaQuery
+export default useMediaQuery
 
-const userMediaQuery = (query: string) => {
-  const [matches, setMatches] = useState(false);
-  useEffect(() => {
-    const media = window.matchMedia(query);
-    if (media.matches != matches) {
-      setMatches(media.matches);
-    }
-    const listener = ()=> setMatches(media.matches);
-    window.addEventListener("resize", listener);
-    return ()=> window.removeEventListener("resize", listener);
-  }, [matches, query]);
-  return matches;
-};
+// const userMediaQuery = (query: string) => {
+//   const [matches, setMatches] = useState(false);
+//   useEffect(() => {
+//     const media = window.matchMedia(query);
+//     if (media.matches != matches) {
+//       setMatches(media.matches);
+//     }
+//     const listener = ()=> setMatches(media.matches);
+//     window.addEventListener("resize", listener);
+//     return ()=> window.removeEventListener("resize", listener);
+//   }, [matches, query]);
+//   return matches;
+// };
 
-export default userMediaQuery
+// export default userMediaQuery
--- src/scences/navbar/navbar.tsx
+++ src/scences/navbar/navbar.tsx
@@ -4,6 +4,8 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
 import Links from "../links";
 import { SelectedPage } from "../shared/types";
+import userMediaQuery from "@/hooks/userMediaQuery";
+import ActionButton from "../shared/ActionButton";
 
 type Props = {
     selectedPage: SelectedPage;
@@ -12,6 +14,8 @@ type Props = {
 
 const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
     const flexBetween = "flex items-center justify-between";
+    const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+    const isAboveMediumScreens = userMediaQuery("(min-width: 1060px)");
     return (
         <nav>
             <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
@@ -20,34 +24,42 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
                         {/* left side */}
                         <img src={Logo} alt="logo" />
                         {/* right side */}
-                        <div className={`${flexBetween} w-full`}>
-                            <div className={`${flexBetween} gap-8 text-sm`}>
-                                <Links
-                                    page={"Home"}
-                                    selectedPage={selectedPage}
-                                    setSelectedPage={setSelectedPage}
-                                />
-                                <Links
-                                    page={"Benefits"}
-                                    selectedPage={selectedPage}
-                                    setSelectedPage={setSelectedPage}
-                                />
-                                <Links
-                                    page={"Our Classes"}
-                                    selectedPage={selectedPage}
-                                    setSelectedPage={setSelectedPage}
-                                />
-                                <Links
-                                    page={"Contact Us"}
-                                    selectedPage={selectedPage}
-                                    setSelectedPage={setSelectedPage}
-                                />
-                            </div>
-                            <div className={`${flexBetween} gap-8`}>
-                                <p>Sign In</p>
-                                <button>Become a Member</button>
-                            </div>
-                        </div>
+                        {isAboveMediumScreens ?
+                            (<div className={`${flexBetween} w-full`}>
+                                <div className={`${flexBetween} gap-8 text-sm`}>
+                                    <Links
+                                        page={"Home"}
+                                        selectedPage={selectedPage}
+                                        setSelectedPage={setSelectedPage}
+                                    />
+                                    <Links
+                                        page={"Benefits"}
+                                        selectedPage={selectedPage}
+                                        setSelectedPage={setSelectedPage}
+                                    />
+                                    <Links
+                                        page={"Our Classes"}
+                                        selectedPage={selectedPage}
+                                        setSelectedPage={setSelectedPage}
+                                    />
+                                    <Links
+                                        page={"Contact Us"}
+                                        selectedPage={selectedPage}
+                                        setSelectedPage={setSelectedPage}
+                                    />
+                                </div>
+                                <div className={`${flexBetween} gap-8`}>
+                                    <p>Sign In</p>
+                                    <ActionButton
+                                        setSelectedPage={setSelectedPage}>Become a Member</ActionButton>
+                                </div>
+                            </div>) : (<div>
+                                <button className="rounded-full bg-secondary-500 p-2"
+                                    onClick={() => setIsMenuToggled(!isMenuToggled)} >
+                                    <Bars3Icon className="h-6 w-6 text-white" />
+                                </button >
+
+                            </div>)}
                     </div>
                 </div>
             </div>
